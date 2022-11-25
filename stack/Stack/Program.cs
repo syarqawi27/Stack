f14db63e7e3ebb34b1c6eef82a66b9a43870cc12
Chickhead <116625946+syarqawi27@users.noreply.github.com>
2022-11-25 11:23:46 +0800

    Add files via upload

---
+++ stack/Stack/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> intStack = new Stack<int>();
+            int topStack;
+
+            intStack.Push(50);
+            intStack.Push(70);
+            intStack.Push(100);
+            intStack.Push(20);
+
+            foreach (int i in intStack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("No.element in stack =" +intStack.Count);
+            Console.WriteLine("......................" );
+            topStack = intStack.Pop();
+
+            foreach (int i in intStack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("No.element in stack =" + intStack.Count);
+            Console.WriteLine("The element Popped =" + topStack);
+            Console.WriteLine("......................");
+            topStack = intStack.Pop();
+
+            foreach (int i in intStack)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("No.element in stack =" + intStack.Count);
+            Console.WriteLine("The element Popped =" + topStack);
+            Console.WriteLine("......................");
+            Console.ReadKey();
+        }
+    }
+}
